36f092267a18c49b4bc9b3fbf8a1560687e5a01e
Timo Sorakivi <timo.sorakivi@gmail.com>
2016-02-12 13:10:41 +0200

    adding

--- week01/testing.R
+++ week01/testing.R
@@ -1,7 +1,10 @@
-# user library ADSFou.. before going on
+# use library ADSFou.. before going on
 library(SDSFoundations)
 bike = BikeData
 table(bike$cyc_freq)
 male_bikers = bike[bike$gender=='M',]
 fem_bikers = bike[bike$gender=='F',]
+fem_bikers$distance
+
+# using git with RStudio
 # http://r-pkgs.had.co.nz/git.html
--- week02/testing_week02.R
+++ week02/testing_week02.R
@@ -1,1 +1,19 @@
-fem_bikers$distance
+# univariate displays
+animaldata = AnimalData
+
+# take 6 first rows to have a generic picture
+head(animaldata)
+
+# plotting one column data, like table(animaldata$Sex) gives amounts of sexes
+plot(animaldata$Sex, main='Bar chart of animal genders', xlab='Gender', ylab='Frequency')
+
+# histogram
+hist(animaldata$Age.Intake)
+
+# arrays of intake ages for females and males
+femaleage = animaldata$Age.Intake[animaldata$Sex == 'Female']
+maleage = animaldata$Age.Intake[animaldata$Sex == 'Male']
+
+# number of bins in histogram
+hist(femaleage, breaks=18)
+
