f0af3049527fe6af6f25187a8351d646b92a9d11
Timo Sorakivi <timo.sorakivi@gmail.com>
2016-03-05 17:15:24 +0200

    add

---
+++ week05/prelab.R
@@ -0,0 +1,16 @@
+WR = WorldRecords
+table(WR$Event)
+
+WR[WR$Athlete == 'Usain Bolt',]
+
+#Subset the data
+menshot <- WR[WR$Event=='Mens Shotput',]
+womenshot <- WR[WR$Event=='Womens Shotput',] 
+
+#Create scatterplots
+plot(menshot$Year,menshot$Record,main='Mens Shotput World Records',xlab='Year',ylab='World Record Distance (m)',pch=16)
+plot(womenshot$Year,womenshot$Record,main='Womens Shotput World Records',xlab='Year',ylab='World Record Distance (m)',pch=16)
+
+#Run linear models
+linFit(menshot$Year, menshot$Record)
+linFit(womenshot$Year,womenshot$Record)
